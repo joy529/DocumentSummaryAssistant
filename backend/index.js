bbbe422626ce91ca73bff78ab15c40dad9797a86
joy jain <joyshah204@gmail.com>
2025-01-18 15:45:50 +0530

    add express dependencie & created well-structured directory for maintainability

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// import express form "express"
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! joy')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
